a6b08f3b5bf2bc66291bfc75dcc9cd0c0cc72799
Damon <dmarcr1997@gmail.com>
2022-07-15 14:05:51 -0400

    Finished up data filtering and orbital period investigation

--- mod.ts
+++ mod.ts
@@ -1,6 +1,10 @@
 import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { parse } from "https://deno.land/std@0.148.0/encoding/csv.ts";
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+interface Planet {
+    [ key : string ] : string 
+}
 
 async function loadPlanetsData() {
     const path = join("data", "kepler_exoplanets_nasa.csv");
@@ -13,9 +17,37 @@ async function loadPlanetsData() {
     };
     const result = await parse(bufReader, options);
     Deno.close(file.rid);
-
-    console.log("The Result: ", result);
+    const planets = (result as Array<Planet>).filter(planet =>{
+        const planetRadius = Number(planet["koi_prad"]);
+        const starsMass = Number(planet["koi_smass"]);
+        const starRadius = Number(planet["koi_srad"]);
+        return planet["koi_disposition"] === "CONFIRMED" 
+            && planetRadius > 0.5 && planetRadius < 1.5 
+            && starsMass > 0.78 && starsMass < 1.04
+            && starRadius > 0.99 && starsMass < 1.01;
+    });
     
+    return planets.map(planet => {
+        return _.pick(planet, [
+            "koi_prad",
+            "koi_smass",
+            "koi_srad",
+            "kepler_name",
+            "koi_count",
+            "koi_steff",
+            "koi_period"
+        ])
+    });    
 }
 
-loadPlanetsData()
+const newEarths = await loadPlanetsData();
+for (const planet of newEarths) {
+    console.log(planet);
+}
+console.log(`${newEarths.length} habitable planets found`)
+
+const longestOrbitalPeriod = Math.max(...newEarths.map(p => p["koi_period"]))
+const shortestOrbitalPeriod = Math.min(...newEarths.map(p => p["koi_period"]))
+
+console.log("Max Orbital Period: " + longestOrbitalPeriod)
+console.log("Min Orbital Period: " + shortestOrbitalPeriod)
